1fdab7cb9467665d0783ee3692e68fc249044c40
yoshiTorisutan-web <creation_tristoune_1@outlook.fr>
2024-01-05 23:02:32 +0100

    first commit

---
+++ src/components/color-picker-header/test.js
@@ -0,0 +1,17 @@
+import { expect } from "chai";
+
+test("color-picker-header color", function (context) {
+  const output = context.render({
+    color: "#000000",
+  });
+
+  expect(output.$("div").attr("style")).to.contain("background-color:#000000");
+});
+
+test("color-picker-header class included", function (context) {
+  const output = context.render({
+    color: "#000000",
+  });
+
+  expect(output.$("div").attr("class")).to.equal("color-picker-header");
+});
